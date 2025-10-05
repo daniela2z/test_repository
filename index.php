cc070e5aeb59485da3ff4814aec9649d1d55683e
daniela2z <144588497+daniela2z@users.noreply.github.com>
2025-10-05 14:26:10 +0300

    Simplify site to hello world heading

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<?php
-echo "hello";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>hello world</title>
+</head>
+<body>
+    <h3>hello world</h3>
+</body>
+</html>
